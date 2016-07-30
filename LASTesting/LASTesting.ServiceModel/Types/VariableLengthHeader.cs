1350bbdca2a97f2205698979f56b25f6679a3b7a
Adam Baxter <voltagex@voltagex.org>
2016-07-30 12:29:31 +1000

    Variable Length Header class

---
+++ LASTesting/LASTesting.ServiceModel/Types/VariableLengthHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LASTesting.ServiceModel.Types
+{
+    public class VariableLengthHeader
+    {
+        ushort Reserved { get; set; }
+        string UserId { get; set; }
+        ushort RecordId { get; set; }
+        ushort RecordIdAfterHeader { get; set; }
+        string Description { get; set; }
+    }
+}
